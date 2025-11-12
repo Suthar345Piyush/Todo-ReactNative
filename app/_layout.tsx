d47567fc34975dfbe50fba522b1e4730fdb864a5
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-11-12 11:03:25 +0530

    setted up _layout according to notifications systems

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useEffect , useRef } from "react";
+import * as Notifications from "expo-notifications";
+
+import { requestNotificationPermission , addNotificationResponseListner } from "@/components/NotificationManager";
+
+
 
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
@@ -10,6 +16,71 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
 
 
 export default function RootLayout() {
+
+  // setting two reference 1. notificationListener , 2. responseListener , using useRef
+
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+
+
+  useEffect(() => {
+
+        // request notification permissions on app startup 
+
+        requestNotificationPermission().then((granted) => {
+           if(granted) {
+             console.log("Notification Permission Granted");
+           } else {
+            console.log('Notification permission denied');
+           }
+        });
+
+        // listen for notification when app is in foreground  
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(
+            (notification) => {
+               console.log("Notification Received:" , notification);
+            }
+        );
+
+
+        // listen when user taps on notification
+
+        responseListener.current = addNotificationResponseListner((response) => {
+           console.log('User tapped notification:', response);
+
+
+           // getting the todo text from notification data 
+
+           const todoText = response.notification.request.content.data?.todoText;
+
+
+           //check the todoText 
+           if(todoText) {
+             console.log("Todo to complete:",todoText);
+           }
+        });
+
+
+
+
+        // cleaning up listeners when component unmount 
+
+        return () => {
+           if(notificationListener.current) {
+               notificationListener.current.remove();
+           }
+
+           if(responseListener.current){
+              responseListener.current.remove();
+           }
+        };
+
+} , []);
+
+
+
   return ( 
   <ConvexProvider client={convex}>
       <ThemeProvider childern = {
